4de2020a30e27b6537673e65d86f938106eea8a4
qtheissen <325957@student.mboutrecht.nl>
2019-11-22 11:43:05 +0100

    initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    //wachttijd tussen schieten
+    public float fireRate = 1f;
+
+    //variabele die bijhoud hoe lang het nog duurt tot je mag schieten
+    private float nextFire = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
